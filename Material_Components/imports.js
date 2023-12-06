80aa446927cdc5e0fea3a1a9fe88218682d9cbd5
lukeplays33 <54947833+lukeplays33@users.noreply.github.com>
2023-12-06 08:46:59 +0000

    added more imports

--- Material_Components/imports.js
+++ Material_Components/imports.js
@@ -1,6 +1,13 @@
 import { initSettings } from './js/initFunctions.js';
 import { generateColors } from './js/materilaColorGen.js';
 
+import { ColorPicker } from './Components/colorPicker.js';
+import { NavigationBar } from './Components/NavigationBar.js';
+import { OptionItem } from './Components/OptionItem.js';
+import { NavigationTabs } from './Components/Tabs.js';
+
+import { openColorPicker } from './Components/functions/colorPicker.js';
+
 generateColors();
 
-export { initSettings }
+export { initSettings, NavigationTabs, OptionItem, NavigationBar, ColorPicker, openColorPicker }
--- Material_Components/js/windowAlternative.js
+++ Material_Components/js/windowAlternative.js
@@ -1,6 +1,6 @@
 let customWindow = {
 	darkTheme: window.matchMedia("(prefers-color-scheme: dark)").matches,
-	themeColour:document.querySelector('meta[name="theme-color"]').getAttribute('content'),
+	themeColour: document.querySelector('meta[name="theme-color"]').getAttribute('content'),
 };
 
 export { customWindow }
