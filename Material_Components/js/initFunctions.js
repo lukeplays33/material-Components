14c0b443d00c25211475a544a813b0ff3cf9b2db
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-01-15 14:12:55 +0000

    fixed new background style on different themes

--- Material_Components/js/initFunctions.js
+++ Material_Components/js/initFunctions.js
@@ -5,6 +5,19 @@ let i;
 function initSettings (json) {
 	for(i of Object.keys(json)) {
 		customWindow[i] = json[i];
+		onChange(i);
+	}
+}
+
+function onChange (key) {
+	if(key == 'darkTheme') {
+		if(customWindow[key]) {
+			document.documentElement.style.backgroundColor = 'black';
+			document.body.style.backgroundColor = 'color-mix( in srgb, var(--md-sys-color-primary-container) 30%, transparent);'
+		} else {
+			document.documentElement.style.backgroundColor = 'white';
+			document.body.style.backgroundColor = 'background-color:color-mix( in srgb, var(--md-sys-color-tertiary-container) 40%, transparent);'
+		}
 	}
 }
 
