bf10bfad424322786b685ac74cb3ca3e9ffe4a8b
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-01-17 16:52:33 +0000

    test

--- Material_Components/js/initFunctions.js
+++ Material_Components/js/initFunctions.js
@@ -21,7 +21,7 @@ function onChange(key) {
 			document.documentElement.style.backgroundColor = 'black';
 			document.body.style.backgroundColor = `rgba(${rgba}, 0.3)`;
 		} else {
-			let rgba = hexToRgb(window.getComputedStyle(r).getPropertyValue('--md-sys-color-tertiary'));
+			let rgba = hexToRgb(window.getComputedStyle(r).getPropertyValue('--md-sys-color-primary'));
 
 			document.documentElement.style.backgroundColor = 'white';
 			document.body.style.backgroundColor = `rgba(${rgba}, 0.2)`;
