da7d5203ac29343a7bcb16882b8483819254a5e2
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-01-18 13:42:27 +0000

    test

--- Material_Components/js/windowAlternative.js
+++ Material_Components/js/windowAlternative.js
@@ -1,6 +1,6 @@
 let customWindow = {
 	darkTheme: window.matchMedia("(prefers-color-scheme: dark)").matches,
-	themeColour: document.querySelector('meta[name="theme-color"]').getAttribute('content') ?? '#008dcd',
+	themeColour:'#008dcd',
 };
 
 export { customWindow }
