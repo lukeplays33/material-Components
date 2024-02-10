6d17a12cb8531bea23dd869363e433eb2b8ecfef
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-02-10 16:26:56 +0000

    testing for same colours as parent in iframe

--- Material_Components/js/windowAlternative.js
+++ Material_Components/js/windowAlternative.js
@@ -1,6 +1,21 @@
+let colour = '#008dcd';
+
+function inIframe () {
+    try {
+        return window.self !== window.top;
+    } catch (e) {
+        return true;
+    }
+}
+
+if(inIframe()) {
+	colour = window.top.document.body;
+	al;ert(colour);
+}
+
 let customWindow = {
 	darkTheme: window.matchMedia("(prefers-color-scheme: dark)").matches,
-	themeColour:'#008dcd',
+	themeColour: colour,
 	colourGeneration:'Sketch'
 };
 
