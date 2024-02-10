5bf1393e0f6e119a1de60fff53b0637a2be63bfa
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-02-10 16:37:54 +0000

    test

--- Material_Components/js/windowAlternative.js
+++ Material_Components/js/windowAlternative.js
@@ -9,7 +9,7 @@ function inIframe () {
 }
 
 if(inIframe()) {
-	colour = window.top.getComputedStyle(document.querySelector(':root')).getPropertyValue('--md-sys-color-source');
+	colour = getComputedStyle(window.top.document.querySelector(':root')).getPropertyValue('--md-sys-color-source');
 	console.log(colour);
 }
 
