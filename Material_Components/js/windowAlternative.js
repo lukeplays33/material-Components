c17ec542e617d7888ec721ccf2f0225aeb9ce4d1
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-02-11 18:44:17 +0000

    added inIframe utils func

--- Material_Components/js/windowAlternative.js
+++ Material_Components/js/windowAlternative.js
@@ -1,22 +1,18 @@
+import { inIframe } from '../utils/iframe.js';
+
 let colour = '#008dcd';
 let themeStyle = 'Sketch';
 
-function inIframe () {
-    try {
-        return window.self !== window.top;
-    } catch (e) {
-        return true;
+window.onload = function () {
+    if (inIframe()) {
+        colour = getComputedStyle(window.top.document.querySelector(':root')).getPropertyValue('--md-sys-color-source');
     }
 }
 
-if(inIframe()) {
-	colour = getComputedStyle(window.top.document.querySelector(':root')).getPropertyValue('--md-sys-color-source');
-}
-
 let customWindow = {
-	darkTheme: window.matchMedia("(prefers-color-scheme: dark)").matches,
-	themeColour: colour,
-	colourGeneration:themeStyle
+    darkTheme: window.matchMedia("(prefers-color-scheme: dark)").matches,
+    themeColour: colour,
+    colourGeneration: themeStyle
 };
 
 export { customWindow }
