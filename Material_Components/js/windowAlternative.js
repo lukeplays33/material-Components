87175c4b0c12a1ec23a16bf64466a90a82ca31c0
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-02-11 18:57:09 +0000

    yrestyryeyr

--- Material_Components/js/windowAlternative.js
+++ Material_Components/js/windowAlternative.js
@@ -4,6 +4,7 @@ let colour = '#008dcd';
 let themeStyle = 'Sketch';
 
 window.self.onload = function () {
+    alert()
     if (inIframe()) {
         colour = getComputedStyle(window.top.document.querySelector(':root')).getPropertyValue('--md-sys-color-source');
         console.log(colour)
