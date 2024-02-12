e9237074a6c5cbdab361a1a3ce106e575c6ddb3f
= <lukehoogenboom@gmail.com>
2024-02-12 18:05:26 +0100

    test

--- Material_Components/js/windowAlternative.js
+++ Material_Components/js/windowAlternative.js
@@ -3,6 +3,7 @@ import { inIframe } from '../utils/iframe.js';
 let colour = '#008dcd';
 let themeStyle = 'Sketch';
 
+alert(window.sessionStorage.themeColour)
 let customWindow = {
     darkTheme: window.matchMedia("(prefers-color-scheme: dark)").matches,
     themeColour: window.sessionStorage.themeColour ?? colour,
