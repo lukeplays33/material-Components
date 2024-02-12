359294897bcaddb1b717ad7269d48cbce2bc816c
= <lukehoogenboom@gmail.com>
2024-02-12 19:34:49 +0100

    test 7

--- Material_Components/js/windowAlternative.js
+++ Material_Components/js/windowAlternative.js
@@ -1,16 +1,17 @@
-import { inIframe } from '../utils/iframe.js';
+import { generateColors } from '../js/materialColorGen.js';
 
 let colour = '#008dcd';
 let themeStyle = 'Sketch';
 
 let customWindow = {
     darkTheme: window.matchMedia("(prefers-color-scheme: dark)").matches,
-    themeColour: window.sessionStorage.themeColour ?? colour,
+    themeColour: colour,
     colourGeneration: themeStyle
 };
 
 window.onstorage = function () {
-    alert()
+    customWindow['themeColour'] = window.sessionStorage.themeColour;
+    generateColors();
 }
 
 export { customWindow }
