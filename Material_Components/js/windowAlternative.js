9f065ce1e4580b24c81d14bad9f0320762292be5
= <lukehoogenboom@gmail.com>
2024-02-12 18:12:08 +0100

    backup

--- Material_Components/js/windowAlternative.js
+++ Material_Components/js/windowAlternative.js
@@ -3,11 +3,14 @@ import { inIframe } from '../utils/iframe.js';
 let colour = '#008dcd';
 let themeStyle = 'Sketch';
 
-alert(window.sessionStorage.themeColour)
 let customWindow = {
     darkTheme: window.matchMedia("(prefers-color-scheme: dark)").matches,
     themeColour: window.sessionStorage.themeColour ?? colour,
     colourGeneration: themeStyle
 };
+
+window.sessionStorage.onchange = function () {
+    customWIndow
+}
 
 export { customWindow }
